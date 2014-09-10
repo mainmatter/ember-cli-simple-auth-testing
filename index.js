8f025beeb71ae4788ccf5c8576f8b8f46c5b3c84
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-09-10 15:26:16 +0200

    load initializer only when testing

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+var path = require('path');
+var fs   = require('fs');
+
+function unwatchedTree(dir){
+  return {
+    read: function() { return dir; }
+  };
+}
+
 module.exports = {
   name: 'Ember CLI Simple Auth Testing',
+
+  treeFor: function(name) {
+    if(!this.app.tests) {
+      return;
+    }
+
+    var treePath = path.join(__dirname, name);
+    if (fs.existsSync(treePath)) {
+      return unwatchedTree(treePath);
+    }
+  },
 
   included: function(app) {
     this._super.included(app);
 
-    this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-testing.amd.js', {
-      type:    'test',
-      exports: {
-        'simple-auth-testing/test-helpers': ['default'],
-        'simple-auth-testing/initializer':  ['default']
-      }
-    });
+    if (app.tests) {
+      this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-testing.amd.js', {
+        exports: {
+          'simple-auth-testing/test-helpers': ['default'],
+          'simple-auth-testing/initializer':  ['default']
+        }
+      });
+    }
   }
 }
