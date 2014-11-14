1ecee94d4d1c14380211d77a4c091e56be93c94d
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-11-14 10:10:57 +0100

    insert helpers into jshintrc ignore list automatically

--- blueprints/ember-cli-simple-auth-testing/index.js
+++ blueprints/ember-cli-simple-auth-testing/index.js
@@ -1,8 +1,16 @@
+var EOL = require('os').EOL;
+
 module.exports = {
   normalizeEntityName: function() {
   },
 
   afterInstall: function() {
+    this.insertIntoFile('tests/.jshintrc',
+      '    "authenticateSession",' + EOL +
+      '    "invalidateSession",',
+      { after: '  "predef": [' + EOL }
+    );
+
     return this.addBowerPackageToProject('ember-simple-auth', '0.7.1');
   }
 };
