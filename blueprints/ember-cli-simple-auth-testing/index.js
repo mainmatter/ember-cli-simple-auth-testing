bdd076572c4ea3bd69c070019a0952066f74cd9d
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-11-14 10:40:50 +0100

    automatically load helpers

--- blueprints/ember-cli-simple-auth-testing/index.js
+++ blueprints/ember-cli-simple-auth-testing/index.js
@@ -10,6 +10,10 @@ module.exports = {
       '    "invalidateSession",',
       { after: '  "predef": [' + EOL }
     );
+    this.insertIntoFile('tests/helpers/start-app.js',
+      "import 'simple-auth-testing/test-helpers';" + EOL + EOL
+      { before: 'export default function startApp(attrs) {' + EOL }
+    );
 
     return this.addBowerPackageToProject('ember-simple-auth', '0.7.1');
   }
