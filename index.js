eff9af66b0b9c325fb6ac4da7c0d3f47e53a1a5b
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-03-15 21:12:07 +0100

    simplified included hook

--- index.js
+++ index.js
@@ -16,10 +16,8 @@ module.exports = {
   },
 
   included: function(app) {
-    this._super.included(app);
-
     if (app.tests) {
-      this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-testing.amd.js', {
+      app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-testing.amd.js', {
         exports: {
           'simple-auth-testing/test-helpers': ['default'],
           'simple-auth-testing/initializer':  ['default']
